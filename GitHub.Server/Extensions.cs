60fc72dc01a38b920e51f8c4e4df0d422b24bba2
chivery <33777604+chivery@users.noreply.github.com>
2018-03-01 11:13:33 -0600

    Initial commit

---
+++ GitHub.Server/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub.Server
+{
+    public static class Extensions
+    {
+        public static string ToHeader(this String value)
+        {
+            string output = null;
+            foreach (char c in value)
+            {
+                output += $"{c} ";
+            }
+            return output.ToUpper();
+        }
+    }
+}
---
+++ GitHub.Server/GitServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub.Server
+{
+    public class GitServer
+    {
+        public static string Hello()
+        {
+            return $"Hello Git! Today is {DateTime.Now.ToShortDateString()}";
+        }
+    }
+}
